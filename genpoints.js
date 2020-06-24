811d3aa3ff4755309e22e665323aaf1dcbdf6cc9
yang zhang <tianna1121@gmail.com>
2020-06-24 09:49:34 +0800

    d

--- genpoints.js
+++ genpoints.js
@@ -10,13 +10,12 @@ fs.readFile('2020_jizhuan.json', (err, data) => {
     var length = points.length;
 	console.log(length);
     for (var i = 0; i < length; i++){
-	    for (var j=0; j < 100; j++){
+	    for (var j=0; j < 30; j++){
       let tmp_la = (parseFloat(points[i].la) + Math.random()/10).toFixed(6);
       let tmp_lo = (parseFloat(points[i].lo) + Math.random()/10).toFixed(6);
       let tmp_coname = points[Math.floor(Math.random() * 10)].co_name;
      // let tmp_coname =  tmpid;
-      //let tmp_addr = points[Math.floor(Math.random() * 10)].addr;
-      //let tmp_addr = points[Math.floor(Math.random() * 10)].addr;
+      let tmp_addr = points[Math.floor(Math.random() * 10)].addr;
       let tmp_powater = points[Math.floor(Math.random() * 10)].po_water;
       let tmp_poair = points[Math.floor(Math.random() * 10)].po_air;
       let tmp_quan = points[Math.floor(Math.random() * 10)].quantity;
@@ -24,7 +23,7 @@ fs.readFile('2020_jizhuan.json', (err, data) => {
       newpoints.push({
         "id": tmpid,
         "co_name": tmp_coname,
-      //  "addr": tmp_addr,
+        "addr": tmp_addr,
         "po_water": tmp_powater,
         "po_air": tmp_poair,
         "lo": tmp_lo,
