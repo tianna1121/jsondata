ce428e9fd0123bc47a12fa3048e41f9c0a8e2b7b
yang zhang <tianna1121@gmail.com>
2020-06-24 09:35:52 +0800

    d

--- genpoints.js
+++ genpoints.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 fs.readFile('2020_jizhuan.json', (err, data) => {
     if (err) throw err;
     let points = JSON.parse(data);
+    let newpoints = [];
     var tmpid = points.length;
     var length = points.length;
 	console.log(length);
@@ -13,25 +14,26 @@ fs.readFile('2020_jizhuan.json', (err, data) => {
       let tmp_la = (parseFloat(points[i].la) + Math.random()/10).toFixed(6);
       let tmp_lo = (parseFloat(points[i].lo) + Math.random()/10).toFixed(6);
       //let tmp_coname = points[Math.floor(Math.random() * 10)].co_name;
-      let tmp_coname =  tmpid;
-      let tmp_addr = points[Math.floor(Math.random() * 10)].addr;
-      let tmp_powater = points[Math.floor(Math.random() * 10)].po_water;
-      let tmp_poair = points[Math.floor(Math.random() * 10)].po_air;
-      let tmp_quan = points[Math.floor(Math.random() * 10)].quantity;
+      //let tmp_coname =  tmpid;
+      //let tmp_addr = points[Math.floor(Math.random() * 10)].addr;
+      //let tmp_addr = points[Math.floor(Math.random() * 10)].addr;
+      //let tmp_powater = points[Math.floor(Math.random() * 10)].po_water;
+      //let tmp_poair = points[Math.floor(Math.random() * 10)].po_air;
+      //let tmp_quan = points[Math.floor(Math.random() * 10)].quantity;
       tmpid++;
-      points.push({
-        "id": tmpid,
-        "co_name": tmp_coname,
-        "addr": tmp_addr,
-        "po_water": tmp_powater,
-        "po_air": tmp_poair,
+      newpoints.push({
+      //  "id": tmpid,
+      //  "co_name": tmp_coname,
+      //  "addr": tmp_addr,
+      //  "po_water": tmp_powater,
+      //  "po_air": tmp_poair,
         "lo": tmp_lo,
         "la": tmp_la,
-        "quantity": tmp_quan,
+      //  "quantity": tmp_quan,
       });
 	    }
     }
-    fs.writeFileSync('points-2.json', JSON.stringify(points));
+    fs.writeFileSync('points-2.json', JSON.stringify(newpoints));
 });
 
 console.log('This is after the read call');
